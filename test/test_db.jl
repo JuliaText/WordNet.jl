706439b402ec4a394cfd3617435b56981873d180
generativist <jbn@abreka.com>
2015-09-21 19:49:24 -0400

    Add test for `path_to_index_file`

--- test/test_db.jl
+++ test/test_db.jl
@@ -1,6 +1,11 @@
-facts("path_to_data_file") do
-    context("returns correct path") do 
+facts("DB") do
+    context("path_to_data_file") do 
         expected = joinpath("MockDB", "dict", "data.verb")
         @fact WordNet.path_to_data_file(DB("MockDB"), 'v') --> expected
+    end
+
+    context("path_to_index_file") do 
+        expected = joinpath("MockDB", "dict", "index.verb")
+        @fact WordNet.path_to_index_file(DB("MockDB"), 'v') --> expected
     end
 end
