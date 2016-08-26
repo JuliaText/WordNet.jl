c56212c33ffe2570eb321cf176f7e80d95a12076
Lyndon White <lyndon.white@research.uwa.edu.au>
2016-08-27 02:12:44 +1000

    Add sensekeys

---
+++ src/sensekeys.jl
@@ -0,0 +1,9 @@
+export sensekey, sensekeys
+
+function sensekey(db::DB, ss::Synset, lem::Lemma)
+    db.sensekeys[(ss.offset,lem.word)]
+end
+
+function sensekeys(db::DB, lem::Lemma)
+    [db.sensekeys[(ss_offset,lem.word)] for ss_offset in lem.synset_offsets]
+end
---
+++ test/test_sensekeys.jl
@@ -0,0 +1,10 @@
+facts("sensekeys") do
+    const mock_db = DB(joinpath(dirname(@__FILE__), "mock_db"))
+
+	
+	lem = mock_db["section",'n']
+	ss = synsets(mock_db, lem)
+	@fact sensekeys(mock_db, lem) --> ["section%1:15:01::"]
+	@fact sensekey(mock_db, ss[1], lem) --> "section%1:15:01::"
+
+end
