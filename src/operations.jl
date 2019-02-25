7c27ccf2f0418cdfddf497bc43b1705bc5c1d882
Michael Boratko <boratko@gmail.com>
2019-02-25 09:22:41 -0500

    Abstracted expanded relation code, made hypernyms work with relation abstraction as a set

--- src/operations.jl
+++ src/operations.jl
@@ -1,29 +1,33 @@
-export synsets, antonyms, hypernyms, hyponyms, expanded_hypernyms
+export synsets, antonyms, hypernyms, hyponyms, expanded_hypernyms, expanded_hyponyms
 
 synsets(db::DB, lemma::Lemma) = map(lemma.synset_offsets) do offset
     db.synsets[lemma.pos][offset]
 end
 
-relation(db::DB, synset::Synset, pointer_sym) = map(
+relation(db::DB, synset::Synset, pointer_sym) = Set(map(
     ptr -> db.synsets[ptr.pos][ptr.offset],
     filter(ptr -> ptr.sym == pointer_sym, synset.pointers)
-)
+))
+
+function expanded_relation(db::DB, synset::Synset, pointer_sym)
+    all_related = Set{Synset}()
+    queue = relation(db, synset, pointer_sym)
+
+    while !isempty(queue)
+        node = pop!(queue)
+        push!(all_related, node)
+        local_relatives = setdiff(relation(db, node, pointer_sym), all_related)
+        delete!(local_relatives, synset)
+        union!(queue, local_relatives)
+    end
+
+    all_related
+end
 
 antonyms(db::DB, synset::Synset)  = relation(db, synset, ANTONYM)
 hyponyms(db::DB, synset::Synset)  = relation(db, synset, HYPONYM)
+hypernyms(db::DB, synset::Synset) = relation(db, synset, HYPERNYM)
 
-function hypernyms(db::DB, synset::Synset)
-    Set(db.synsets[synset.synset_type][ptr.offset] for ptr ∈ synset.pointers if ptr.sym == HYPERNYM)
-end
 
-function expanded_hypernyms(db::DB, synset::Synset)
-    path = Vector{Synset}()
-
-    node = hypernyms(db, synset)
-    while !is_nothing(node)
-        push!(path, node)
-        node = hypernyms(db, node)
-    end
-
-    path
-end
+expanded_hyponyms(db::DB, synset::Synset) = expanded_relation(db, synset, HYPONYM)
+expanded_hypernyms(db::DB, synset::Synset) = expanded_relation(db, synset, HYPERNYM)
